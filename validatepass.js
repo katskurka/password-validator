0addb29371c16646c9bb774f1d7c6ae16fb7d25e
katskurka <kati.skurka@gmail.com>
2021-07-15 11:01:26 -0400

    added password length check function

--- validatepass.js
+++ validatepass.js
@@ -1,3 +1,23 @@
 function validatePassword(password) {
   if (password.length >= 8 && 
 }
+
+function passwordLength(password) {
+  if (password.length >= 8) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function passwordLowerCase() {
+
+}
+
+function passwordUpperCase() {
+
+}
+
+function passwordSpecialChar() {
+
+}
