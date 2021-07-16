30b0e9bf184a77357330fe27597b0c58e10ab5a2
katskurka <kati.skurka@gmail.com>
2021-07-16 13:13:08 -0400

    tweaked hasLowerCase fx and got 4/6 passing tests

--- validatepass.js
+++ validatepass.js
@@ -1,19 +1,26 @@
-function validatePassword(password) {
-  if (password.length >= 8 && hasLowerCase === true && hasUpperCase === true && hasSpecialChar === true) return true
+function correctLength(password) {
+  if (password.length >= 8) {
+    return true
+  }
+  else {
+    return false
+  }
 }
-
 function hasLowerCase(password) {
   for (let char = 0; char < password.length; char++) {
-    if (char === char.toLowerCase()) break
+    if (password[char] === password[char].toLowerCase()) {
+      return true
+    }
+    else {
+      return false
+    }
   }
-
-  return true
 }
 
 function hasUpperCase(password) {
   for (let char = 0; char < password.length; char++) {
     // let currentChar = password [i]  should this be added?
-    if (char === char.toUpperCase()) break
+    if (password[char] === password[char].toUpperCase()) break
   }
 
   return true
@@ -23,8 +30,34 @@ function hasSpecialChar(password) {
   let specialChars = '!@#$%^&*?><+='
 
   for (let char = 0; char < password.length; char++) {
-    if (char === char.includes(specialChars)) break
+    if (password[char] === password[char].includes(specialChars)) break
   }
 
   return true
 }
+
+function hasNumber(password) {
+  for (let char = 0; char < password.length; char++) {
+    if (password[char] === !NaN) break
+  }
+
+  return true
+}
+
+function validatePassword(password) {
+  let passwordLength = false
+  let lowerCase = false
+  let upperCase = false
+  let specialCharacter = false
+  let checkNumber = false
+
+  passwordLength = correctLength(password)
+  lowerCase = hasLowerCase(password)
+  upperCase = hasUpperCase(password)
+  specialCharacter = hasSpecialChar(password)
+  checkNumber = hasNumber(password)
+
+  return passwordLength && lowerCase && upperCase & specialCharacter && checkNumber
+}
+
+module.exports = validatePassword
