ee8377d896e1559699d6677c0cd76db0f8928399
katskurka <kati.skurka@gmail.com>
2021-07-18 17:29:11 -0400

    refactored original code into chained if statements

--- validatepass.js
+++ validatepass.js
@@ -1,3 +1,38 @@
+function validatePassword(password) {
+  let lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  let upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let specialChars = '!@#$%^&*?><+='
+  let checkNumbers = '0123456789'
+  let correctLength = false
+  let isLower = false
+  let isUpper = false
+  let hasSpecial = false
+  let isNumber = false
+
+  for (let char = 0; char < password.length; char++) {
+    if (password.length >= 8) {
+      correctLength = true
+    }
+    if (lowerCaseLetters.includes(password[char])) {
+      isLower = true
+    }
+    if (upperCaseLetters.includes(password[char])) {
+      isUpper = true
+    }
+    if (specialChars.includes(password[char])) {
+      hasSpecial = true
+    }
+    if (checkNumbers.includes(password[char])) {
+      isNumber = true
+    }
+  }
+
+  return correctLength && isLower && isUpper && hasSpecial && isNumber
+}
+
+module.exports = validatePassword
+
+/* Original code before refactoring: 
 function correctLength(password) {
   if (password.length >= 8) {
     return true
@@ -32,18 +67,6 @@ function hasUpperCase(password) {
   return isUpper
 }
 
-/* function hasUpperCase(password) {
-  let isUpper = false
-
-  for (let char = 0; char < password.length; char++) {
-    if (password[char] === password[char].toUpperCase()) {
-      isUpper = true
-    }
-  }
-
-  return isUpper
-} */
-
 function hasSpecialChar(password) {
   let specialChars = '!@#$%^&*?><+='
   let hasSpecial = false
@@ -75,4 +98,4 @@ function validatePassword(password) {
   hasSpecialChar(password) && hasNumber(password)
 }
 
-module.exports = validatePassword
+module.exports = validatePassword */
